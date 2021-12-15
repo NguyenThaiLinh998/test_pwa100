1a62b8fa3a1276949c5938a347d7ce9ad6765ad8
thái linh nguyễn <nguyenthailinh1111998@gmail.com>
2021-12-15 16:29:24 +0700

    first commit

---
+++ dist/precache-manifest.710d031c02e8684e83194177b01180c4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9891d5e1fcb917d07df963b89bb10037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95fd901eb5f24d39c58e",
+    "url": "/static/css/4.60ba142b.chunk.css"
+  },
+  {
+    "revision": "55ea6d576cb9259c6d78",
+    "url": "/static/css/7.a1ac3e9c.chunk.css"
+  },
+  {
+    "revision": "46b464bcb20d977628dd",
+    "url": "/static/css/8.539229fb.chunk.css"
+  },
+  {
+    "revision": "792f0d84edc5f5e80807",
+    "url": "/static/css/9.33057e79.chunk.css"
+  },
+  {
+    "revision": "472c559fed5d7a516dc9",
+    "url": "/static/css/main.54f01ea6.chunk.css"
+  },
+  {
+    "revision": "e04853d53da3f9cb45a5",
+    "url": "/static/js/0.d533829c.chunk.js"
+  },
+  {
+    "revision": "78742329766e422bb134",
+    "url": "/static/js/1.e6dcfe0a.chunk.js"
+  },
+  {
+    "revision": "95fd901eb5f24d39c58e",
+    "url": "/static/js/4.6eddd891.chunk.js"
+  },
+  {
+    "revision": "f239cc04571ce07ee5e6657051470c75",
+    "url": "/static/js/4.6eddd891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a670ca4e9908f2d593",
+    "url": "/static/js/5.84409c7b.chunk.js"
+  },
+  {
+    "revision": "bbfc68a2a31d4a4c1469",
+    "url": "/static/js/6.2b87e4ab.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/6.2b87e4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ea6d576cb9259c6d78",
+    "url": "/static/js/7.3f18f53e.chunk.js"
+  },
+  {
+    "revision": "46b464bcb20d977628dd",
+    "url": "/static/js/8.569ff724.chunk.js"
+  },
+  {
+    "revision": "792f0d84edc5f5e80807",
+    "url": "/static/js/9.7a018724.chunk.js"
+  },
+  {
+    "revision": "472c559fed5d7a516dc9",
+    "url": "/static/js/main.03a5865a.chunk.js"
+  },
+  {
+    "revision": "d18bb455e51b3c6b5f3b",
+    "url": "/static/js/runtime-main.a532d0df.js"
+  },
+  {
+    "revision": "23d5a568ada312f4909e31068f5d1b7a",
+    "url": "/static/media/avatar.23d5a568.svg"
+  }
+]);
